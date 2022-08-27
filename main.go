91da30b019b82661b5cc8afd663c31b0a6d72440
OlGe <klardoch@gmail.com>
2022-08-27 15:45:35 +0200

    added hello world webserver

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"io"
+	"log"
+	"net/http"
 )
 
-func timer() {
-	counter := 0
-
-	for {
-		if true {
-			time.Sleep(1 * time.Second)
-			counter++
-			if counter%5 == 0 {
-				fmt.Printf("Hello World! I was born %d seconds ago. \n", counter)
-			}
-		}
-	}
-}
+// func timer(counter int) {
+// 	for {
+// 		if true {
+// 			time.Sleep(1 * time.Second)
+// 			counter++
+// 			if counter%5 == 0 {
+// 				s := fmt.Sprintf("Hello World! I was born %d seconds ago. \n", counter)
+// 				return s
+// 			}
+// 		}
+// 	}
+// }
 
 func main() {
-	timer()
+	// Hello world, the web server
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
